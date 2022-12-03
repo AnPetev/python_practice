de24a8a6a6cfa80131b88d10498677bc24439922
AnPetev <93883506+AnPetev@users.noreply.github.com>
2022-12-03 19:35:53 +0400

    Add files via upload

--- C++/vector.cpp
+++ C++/vector.cpp
@@ -1,134 +1,132 @@
 #include <iostream>
 
-class Vector
-{
-      private:
-         int size;
-         int capacity;
-         int* arr;
-      public:
-           Vector();
-           Vector(int size, int* tmparr);
-          ~Vector();
-           Vector(const Vector& obj);
-           Vector(Vector&& oth);
-           void push_back(int elem);
-           Vector& operator=(const Vector& ob);
-           Vector operator+(const Vector& v1);
-           void print();
-           int& operator[](int index);
-           
+class Vector {
+private:
+	int size;
+	int capacity;
+	int* arr;
+public:
+	Vector();
+	Vector(int size, int* data);
+	Vector(const Vector& other);
+	Vector(Vector&& obj);
+    ~Vector();
+	Vector& operator = (const Vector& obj);
+	Vector operator+(const Vector& v);
+	void push_back(int elem);
+	void print();
+
+
+
 };
+Vector::Vector() :size(0), capacity(0), arr(nullptr) {
+	std::cout << "default ctor has called" << std::endl; }
 
-
-Vector::Vector():size(0), capacity(0), arr(nullptr){std::cout<<"default ctor has called"<<std::endl;}
-
-Vector::Vector(int size, int* tmparr):size(size)
-{
-      capacity = size * 2;
-      this->arr = new int[capacity];
-      for(int i = 0; i < size; i++){
-            this->arr[i] = tmparr[i];
-      }
-      std::cout<<"ctor has called"<<std::endl;
+Vector::Vector(int size, int* data) {
+	this->size = size;
+	capacity = size * 2;
+	this->arr = new int[capacity];
+	for (int i = 0; i < size; i++) {
+		this->arr[i] = data[i];
+	}
+	std::cout << " ctor has called" << std::endl;
 }
 
-Vector::~Vector()
-{
-      delete[] arr;
-      std::cout<<"dtor has called"<<std::endl;
+Vector::Vector(const Vector& other) {
+	this->size = other.size;
+	this->capacity = other.capacity;
+	this->arr = new int[other.capacity];
+	for (int i = 0; i < size; i++) {
+		this->arr[i] = other.arr[i];
+	}
+	std::cout << "copy ctor has called" << std::endl;
 }
 
-Vector::Vector(const Vector& obj)
-{
-      this->size = obj.size;
-      this->capacity = obj.capacity;
-      this->arr = new int[obj.capacity];
-      for(int i = 0;  i < size; i++)
-      {
-            this->arr[i] = obj.arr[i];
-      }
-      std::cout<<"copy ctor has called"<<std::endl;
+Vector::Vector(Vector&& obj) {
+	this->size = obj.size;
+	this->capacity = obj.capacity;
+	this->arr = obj.arr;
+	obj.size = 0;
+	obj.capacity = 0;
+	obj.arr = nullptr;
+	std::cout << "move ctor has called" << std::endl;
 }
 
-Vector::Vector(Vector&& other)
-{
-      this->size = other.size;
-      this->capacity = other.capacity;
-      this->arr = other.arr;
-      other.size = 0;
-      other.capacity = 0;
-      other.arr = nullptr;
-      std::cout<<"move ctor has called"<<std::endl;
+
+Vector& Vector::operator = (const Vector& obj) {
+	if (this == &obj) {
+		return *this;
+	}
+	delete[] arr;
+	this->size = obj.size;
+	this->capacity = obj.capacity;
+	arr = new int[capacity];
+	for (int i = 0; i < size; i++) {
+		arr[i] = obj.arr[i];
+	}
+	std::cout << "op= has called" << std::endl;
+	return *this;
+	
+
 }
 
-void Vector::push_back(int elem)
-{
-      if(size == 0){
-            capacity = 1;
-            arr = new int[capacity];
-      }
-      else if(size == capacity){
-            capacity = size * 2;
-            int* tmp = new int[capacity];
-            for(int i = 0; i < size; i++){
-                  tmp[i] = arr[i];
-            }
-      delete[] arr;
-      arr = tmp;
-      }
-      arr[size] = elem;
-      ++size;
+Vector Vector::operator+(const Vector& v) {
+	Vector r;
+	r.size = this->size + v.size;
+	r.capacity = this->capacity + v.capacity;
+	r.arr = new int[r.capacity];
+	for (int i = 0; i < this->size; i++) {
+		r.arr[i] = this->arr[i] + v.arr[i];
+	}
+	for (int i = 0; i < v.size; i++) {
+		r.arr[i] = this->arr[i] + v.arr[i];
+	}
+	std::cout << "op+ has called" << std::endl;
+	return r;
+	
+
 }
 
-Vector& Vector::operator=(const Vector& ob)
-{
-      if(this == &ob){
-            return *this;
-      }
-      delete[] arr;
-      this->size = ob.size;
-      this->capacity = ob.capacity;
-      arr = new int[capacity];
-      for(int i = 0; i < size; i++){
-            arr[i] = ob.arr[i];
-      }
-      return *this;
-      std::cout<<"op= has called"<<std::endl;
+void Vector::push_back(int elem) {
+	if (size == 0) {
+		capacity = 1;
+		arr = new int[capacity];
+	}
+	else if (size == capacity) {
+		capacity = size * 2;
+		int* tmp = new int[capacity];
+		for (int i = 0; i < size; i++) {
+			tmp[i] = arr[i];
+		}
+		delete[] arr;
+		arr = tmp;
+	}
+	arr[size] = elem;
+	size++;
 }
 
-Vector Vector::operator+(const Vector& v)
-{
-      Vector r;
-      r.size = this->size + v.size;
-      r.capacity = this->capacity + v.capacity;
-      r.arr = new int[r.capacity];
-      for(int i = 0; i < this->size; i++){
-            r.arr[i] = this->arr[i] + v.arr[i];
-      }
-      for(int i = 0; i < v.size; i++){
-            r.arr[i] = this->arr[i] + v.arr[i];
-      }
-      return r;
+Vector::~Vector() {
+	delete[] arr;
+	std::cout << "dtor has called" << std::endl;
 }
-
-void Vector::print()
-{
-      for(int i = 0; i < size; i++){
-            std::cout<<arr[i]<<" ";
-      }
-      std::cout<<std::endl;
+void Vector::print() {
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
 }
-
-int& Vector::operator[](int index)
-{
-     return arr[index];
+int main() {
+	int data[] = { 1,2,3,5 };
+	int data2[] = { 4,5,6,9 };
+	Vector v(4, data);
+	//Vector v2(4, data2);
+	//Vector v3(v2);
+	//v2 = v;
+	//v2.print();
+	//Vector v3;
+	//v.operator+(v2).print();
+	Vector v1(4, data2);
+	Vector v3;
+	v3 = v + v1;
+	v3.print();
 }
-int main()
-{
-     int data[] = {1,2,3,4,5};
-     Vector v1(5, data);
-     int s[] = {6,7,8};
-     Vector v2(3, s);
-     v1.operator+(v2).print();
-}   
